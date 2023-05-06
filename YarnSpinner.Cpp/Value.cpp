27a4fe03c3cfc16f1da23cd115c5312fa8fde51f
JoshLee0915 <joshlee0915@gmail.com>
2023-05-05 23:23:32 -0600

    Setup basic ToString implementation for value

--- YarnSpinner.Cpp/Value.cpp
+++ YarnSpinner.Cpp/Value.cpp
@@ -7,5 +7,19 @@ int Yarn::Value::CompareTo(std::any obj)
 
 std::string Yarn::Value::ToString()
 {
-    return "";
+    // TODO: might need to any_cast string
+    std::string value = "unknown";
+    if(const auto v = std::any_cast<int>(InternalValue))
+        value = std::to_string(v);
+    if(const auto v = std::any_cast<unsigned int>(InternalValue))
+        value = std::to_string(v);
+    else if(const auto v = std::any_cast<float>(InternalValue))
+        value = std::to_string(v);
+    else if(const auto v = std::any_cast<double>(InternalValue))
+        value = std::to_string(v);
+    else if(const auto v = std::any_cast<bool>(InternalValue))
+        value = std::to_string(v);
+    else if(const auto v = std::any_cast<char*>(InternalValue))
+        value = std::string(v);
+    return "[Value: type=" + Type()->Name() + ", value=" + value + "]";
 }
